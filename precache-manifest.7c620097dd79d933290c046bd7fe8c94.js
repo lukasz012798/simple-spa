a249fd4da5402850e6a554bc5efe6351cf2f607a
Lukasz Adamczyk <lukasz012798@gmail.com>
2020-05-07 10:23:42 +0200

    Updates

---
+++ precache-manifest.7c620097dd79d933290c046bd7fe8c94.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8e8b7ec156ae3893d8827ff88cc3930",
+    "url": "/simple-spa/index.html"
+  },
+  {
+    "revision": "c2b6be5c03079b8d1c24",
+    "url": "/simple-spa/static/css/main.fcac3665.chunk.css"
+  },
+  {
+    "revision": "182788f5f63c55ecfdc0",
+    "url": "/simple-spa/static/js/2.be325a54.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/simple-spa/static/js/2.be325a54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2b6be5c03079b8d1c24",
+    "url": "/simple-spa/static/js/main.beeae09a.chunk.js"
+  },
+  {
+    "revision": "060f1881f9cf3c5f74eb",
+    "url": "/simple-spa/static/js/runtime-main.19f558d7.js"
+  },
+  {
+    "revision": "f1eb474b9d04ee6038e353307ae2d4b8",
+    "url": "/simple-spa/static/media/road-1209369_1280.f1eb474b.jpg"
+  },
+  {
+    "revision": "879a50bbe000a1dce9c87d1631b2daef",
+    "url": "/simple-spa/static/media/taxi-cab-381233_1280.879a50bb.jpg"
+  },
+  {
+    "revision": "e69353938011fec860f93d278dcdc01d",
+    "url": "/simple-spa/static/media/traffic-3612474_1280.e6935393.jpg"
+  }
+]);
